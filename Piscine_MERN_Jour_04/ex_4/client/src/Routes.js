3d747559381cb664b9ebee8666440397797685fc
Victor Reyes <victor.reyes@hotmail.fr>
2020-05-09 13:54:12 +0200

    Jour 4 - ex_4 - OK

---
+++ Piscine_MERN_Jour_04/ex_4/client/src/Routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+import Register from './auth/Register';
+import Login from './auth/Login';
+import Blog from './core/Blog';
+import BlogEdit from './core/BlogEdit';
+import BlogDetails from './core/BlogDetails';
+import Admin from './core/Admin';
+import AdminRoute from './auth/AdminRoute';
+import PrivateRoute from './auth/PrivateRoute';
+import LoginRoute from './auth/LoginRoute';
+import { Provider } from 'react-redux'
+import store from './store'
+
+const Routes = () => {
+  return (
+    // <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={App} />
+          <Route path='/register' exact component={Register} />
+          <Route path='/login' exact component={Login} />
+          <PrivateRoute path='/:login' exact component={Blog} />
+          <PrivateRoute path='/:user_id/:_id/edit' component={BlogEdit} />
+          <PrivateRoute path='/:login/:_id/' component={BlogDetails} />
+          <AdminRoute path='/admin' exact component={Admin} />
+        </Switch>
+      </BrowserRouter>
+    // </Provider>
+  );
+};
+
+export default Routes;
---
+++ Piscine_MERN_Jour_04/ex_4/client/src/core/Layout.js
@@ -0,0 +1,66 @@
+import React, { Fragment } from "react";
+import { Link, withRouter } from 'react-router-dom'
+import { isAuth, logout } from "../auth/helpers";
+
+const Layout = ({ children, match, history }) => {
+  const isActive = path => {
+    // console.log(match.path);
+    
+    if (match.path === path) {
+      return { cursor: 'pointer', color: '#000000' };
+    } else {
+      return { cursor: 'pointer', color: '#ffffff' };
+    }
+  }
+
+  const nav = () => (
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link to="/" className="nav-link" style={isActive('/')}>Home</Link>
+        {/* {JSON.stringify(match)} */}
+      </li>
+      {/* //if not loggedin, then (&&) */}
+      {/* {!isAuth() && */}
+
+      {isAuth() && isAuth().type === true && (
+        <li className="nav-item">
+          <Link to="/admin" className="nav-link" style={isActive('/admin')}>ADMINS-ONLY</Link>
+        </li>
+      )}
+
+      {isAuth() ?
+        <Fragment>
+          <li className="nav-item">
+            <Link to={'/' + isAuth().login} className="nav-link" style={isActive(isAuth().login)}>{isAuth().login}</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" style={isActive('/login')}
+              onClick={() => {
+                logout(() => {
+                  history.push('/');
+                });
+              }}
+            >Logout</Link>
+          </li>
+        </Fragment>
+        :
+        <Fragment>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link" style={isActive('/login')}>Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-link" style={isActive('/register')}>Register</Link>
+          </li>
+        </Fragment>
+      }
+    </ul>
+  );
+  return (
+    <Fragment>
+      {nav()}
+      <div className="container">{children}</div>
+    </Fragment>
+  );
+};
+
+export default withRouter(Layout);
